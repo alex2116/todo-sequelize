3758d7abc3748581c67ce3fc28874d009c2b6815
alex2116 <reingeld116@gmail.com>
2021-08-20 20:01:36 +0800

    add express-session & add login feature via passport

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const brypt = require('bcryptjs')
+const bcrypt = require('bcryptjs')
+const session = require('express-session')
+const usePassport = require('./config/passport')
+const passport = require('passport')
 
 const app = express()
 const PORT = 3000
@@ -13,8 +16,14 @@ const User = db.User
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+usePassport(app)
 
 app.get('/', (req, res) => {
   return Todo.findAll({
@@ -34,10 +43,6 @@ app.get('/todos/:id', (req, res) => {
 
 app.get('/users/login', (req, res) => {
   res.render('login')
-})
-
-app.post('/users/login', (req, res) => {
-  res.send('login')
 })
 
 app.get('/users/register', (req, res) => {
@@ -68,6 +73,11 @@ app.post('/users/register', (req, res) => {
       .catch(err => console.log(err))
   })
 })
+
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect:'/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/logout', (req, res) => {
   res.send('logout')
